224c4f8b05a85a413d2156495f84cdf5388222be
Supportiii2 <91149905+Supportiii2@users.noreply.github.com>
2021-10-06 22:26:19 +0200

    Update models.py

--- models.py
+++ models.py
@@ -81,7 +81,7 @@ class Post(BaseModel):
         mentions.append(new_mention.lower())
         self.set_scope_mentions(set(mentions))
 
-def can_be_accessed_by(self, user: types.User):
-    return user.id in whitelist
+    def can_be_accessed_by(self, user: types.User):
+        return user.id in whitelist
 
 db.create_tables([User, Post], safe = True)
